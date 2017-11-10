217e92a92ebc52bfc3a8c54d221ebdefff8f448e
Tobias Latzke <centaurus@xcoins.de>
2017-11-10 18:00:59 +0100

    bring back sample harness with get/set instead of read/write

--- chaincode/hands-on/hands-on.go
+++ chaincode/hands-on/hands-on.go
@@ -1,4 +1,3 @@
-// chaincode for hyperledger fabric 1.0
 package main
 
 import (
@@ -8,93 +7,93 @@ import (
 	"github.com/hyperledger/fabric/protos/peer"
 )
 
-// Chaincode contains the state information that is not persisted on the blockchain
+var logger = shim.NewLogger("MYCC")
+
+// Chaincode is the main type, implementing the chaincode interface mandatory methods
 type Chaincode struct {
 }
 
-var logger = shim.NewLogger("MYCC")
-
+// main function starts up the chaincode in the container during instantiate
 func main() {
-	cc := new(Chaincode)
-	err := shim.Start(cc)
-	if err != nil {
-		logger.Error("Error starting chaincode:", err)
+	if err := shim.Start(new(Chaincode)); err != nil {
+		fmt.Printf("Error starting Chaincode: %s", err)
 	}
 }
 
 func debug(format string, a ...interface{}) {
 	logger.SetLevel(shim.LogDebug)
-	logger.Debugf(format, a...)
-}
-
-func error(format string, a ...interface{}) peer.Response {
 	msg := fmt.Sprintf(format, a...)
-	logger.Error(msg)
-	return shim.Error(msg)
+	logger.Debug(msg)
 }
 
-// Init is one of two mandatory exported methods for valid chaincode.
-// It is called on deployment.
+// Init is called during chaincode instantiation to initialize any
+// data. Note that chaincode upgrade also calls this function to reset
+// or to migrate data.
 func (t *Chaincode) Init(stub shim.ChaincodeStubInterface) peer.Response {
 	// Get the args from the transaction proposal
 	args := stub.GetStringArgs()
 	if len(args) != 0 {
-		return error("Incorrect arguments. Constructor doesn't expect arguments!")
+		return shim.Error("Incorrect arguments. Constructor doesn't expect arguments!")
 	}
 	logger.Info("successfully initialized")
 	return shim.Success(nil)
 }
 
-// Invoke is one of three mandatory exported methods for valid chaincode.
-// It is called to modify the world state.
+// Invoke is called per transaction on the chaincode. Each transaction is
+// either a 'get' or a 'set' on the given key.
 func (t *Chaincode) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
 	// Extract the function and args from the transaction proposal
 	fn, args := stub.GetFunctionAndParameters()
 	debug("try to call function '%s' with %d args", fn, len(args))
 
-	// reading access
-	if fn == "read" {
-		return t.read(stub, args)
-		// writing access
-	} else if fn == "write" {
-		return t.write(stub, args)
-	}
-
-	return error("Received unknown function invocation: %s", fn)
-}
-
-func (t *Chaincode) read(stub shim.ChaincodeStubInterface, args []string) peer.Response {
-	if len(args) != 1 {
-		return error("Incorrect number of arguments. Expecting 1: property key")
-	}
-	key := args[0]
-
-	debug("try to read property with key '%s'", key)
-	bytes, err := stub.GetState(key)
-
-	if err == nil {
-		debug("read value: %s", bytes)
+	var result string
+	var err error
+	if fn == "set" {
+		result, err = set(stub, args)
+	} else if fn == "get" {
+		result, err = get(stub, args)
 	} else {
-		debug("reading failed: %v", err)
+		err = fmt.Errorf("unknown function call: %s", fn)
+	}
+	if err != nil {
+		return shim.Error(err.Error())
 	}
 
-	return shim.Success(bytes)
+	// Return the result as success payload
+	return shim.Success([]byte(result))
 }
 
-func (t *Chaincode) write(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+// Set stores the key value pair on the ledger. If the key exists,
+// it will override the value with the new one
+func set(stub shim.ChaincodeStubInterface, args []string) (string, error) {
 	if len(args) != 2 {
-		return error("Incorrect number of arguments. Expecting 2: property key, new value")
+		return "", fmt.Errorf("Incorrect arguments. Expecting a key and a value")
 	}
-	key := args[0]
+
+	debug("try to write property with key '%s'", args[0])
 	bytes := []byte(args[1])
-
-	debug("try to write property with key '%s'", key)
-	err := stub.PutState(key, bytes)
-
+	err := stub.PutState(args[0], bytes)
 	if err != nil {
-		return error("writing failed: %v", err)
+		return "", fmt.Errorf("Failed to set value for key '%s' with error: %s", args[0], err)
+	}
+	debug("written value: %s", bytes)
+	return args[1], nil
+}
+
+// Get returns the value of the specified key
+func get(stub shim.ChaincodeStubInterface, args []string) (string, error) {
+	if len(args) != 1 {
+		return "", fmt.Errorf("Incorrect arguments. Expecting a key")
 	}
 
-	debug("written value: %s", bytes)
-	return shim.Success(bytes)
+	debug("try to read property with key '%s'", args[0])
+	value, err := stub.GetState(args[0])
+	if err != nil {
+		return "", fmt.Errorf("Failed to get value for key: '%s' with error: %s", args[0], err)
+	}
+	if value == nil {
+		return "", fmt.Errorf("Key not found: %s", args[0])
+	}
+	debug("read value: %s", value)
+	return string(value), nil
 }
